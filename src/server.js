24e7da4c432d12fcf280f0d59d09efb2797194b2
lilamckenna <lila.20@dartmouth.edu>
2019-05-24 08:17:12 -0400

    websocket SA done

--- src/server.js
+++ src/server.js
@@ -3,9 +3,22 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+import socketio from 'socket.io';
+import http from 'http';
+import * as Notes from './controllers/note_controller';
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/notes';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
+
 
 // initialize
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // enable/disable cross origin resource sharing if necessary
 app.use(cors());
@@ -26,16 +39,78 @@ app.set('views', path.join(__dirname, '../src/views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// additional init stuff should go before hitting the routing
 
 // default index route
 app.get('/', (req, res) => {
   res.send('hi');
 });
 
+
 // START THE SERVER
 // =============================================================================
 const port = process.env.PORT || 9090;
-app.listen(port);
+server.listen(port);
+
 
 console.log(`listening on: ${port}`);
+// at the bottom of server.js
+// lets register a connection listener
+
+io.on('connection', (socket) => {
+  // on first connection emit notes
+  Notes.getNotes().then((result) => {
+    socket.emit('notes', result);
+  });
+
+  // pushes notes to everybody
+  const pushNotes = () => {
+    Notes.getNotes().then((result) => {
+      // broadcasts to all sockets including ourselves
+      io.sockets.emit('notes', result);
+    });
+  };
+
+  // creates notes and
+  socket.on('createNote', (fields) => {
+    Notes.createNote(fields).then((result) => {
+      pushNotes();
+    }).catch((error) => {
+      console.log(error);
+      socket.emit('error', 'create failed');
+    });
+  });
+  // on update note do what is needful
+  socket.on('updateNote', (id, fields) => {
+    Notes.updateNote(id, fields).then(() => {
+      pushNotes();
+    });
+  });
+
+
+  // on deleteNote do what is needful
+  socket.on('deleteNote', (id) => {
+  // you can do it
+  });
+});
+
+
+// // eslint-disable-next-line no-unused-vars
+// const pushNotesSmoothed = () => {
+//   Notes.getNotes().then((result) => {
+//     emitToSelf(result);
+//     emitToOthers(result);
+//   });
+// };
+
+
+// // add these at the top of your: io.on('connection' section
+// let emitToSelf = (notes) => {
+//   io.emit('notes', notes);
+// };
+// emitToSelf = debounce(emitToSelf, 200);
+
+// let emitToOthers = (notes) => {
+//   io.broadcast.emit('notes', notes);
+// };
+
+// emitToOthers = throttle(emitToOthers, 25);
